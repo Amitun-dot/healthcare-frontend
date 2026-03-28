7287c3fe67076a45fd24a65923af8b8cd5b6cf24
Amitun <amitiuse@gmail.com>
2026-03-28 15:07:21 +0530

    fix vite issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5174,        // ✅ fixed port
-    strictPort: true   // ❗ prevents auto-changing to 5175, 5176, etc.
-  }
-})
+    port: 5174,
+    strictPort: true,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: Number(process.env.PORT) || 4173,
+    allowedHosts: ["my-healthcare.up.railway.app"],
+  },
+});
